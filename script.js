e0cf7121eee6f0b36c3d1db4a0932ad9b3c45f5f
VaderSteve76 <alverson.s@yahoo.com>
2020-05-01 19:36:26 -0400

    Added Event Listeners

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const random = document.getElementById('random');
 const mealsEl = document.getElementById('meals');
 const resultHeading = document.getElementById('result-heading');
 const single_mealEl = document.getElementById('single-meal');
+
+// Event Listeners
+submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
+
+mealsEl.addEventListener('click', e => {
+  const mealInfo = e.composedPath.find(item => {
+    if(item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+  if(mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealById(mealID);
+  }
+});
